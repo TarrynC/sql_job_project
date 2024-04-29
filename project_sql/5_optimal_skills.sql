ceaa755bb839dce36d6749b9211c0efaed0a207a
Tarryn <tarzz.cam@gmail.com>
2024-04-29 20:06:19 +0200

    added query to find most optimal skills

---
+++ project_sql/5_optimal_skills.sql
@@ -0,0 +1,81 @@
+/*
+Q: What are the most optimal skills to learn? (in demand and high paying)
+    - identify skills in high demand which are also associated with high pay for data analyst roles
+    - Concentrate on my area/remote with specified salries
+    - Why? Targets skills that offer job security (high demand) and financial benefits (high salary)
+        offers strategic insights for career development in data analysis
+*/
+
+-- Create two CTEs from previous queries
+WITH skills_demand AS (
+-- Query 3 modified
+    SELECT 
+        skills_dim.skill_id,
+        skills_dim.skills,
+        COUNT(skills_job_dim.job_id) AS demand_count
+    FROM 
+        job_postings_fact
+    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON skills_dim.skill_id = skills_job_dim.skill_id
+    WHERE
+        job_title_short = 'Data Analyst'
+        AND salary_year_avg IS NOT NULL
+        AND (job_work_from_home = True OR job_location LIKE 'Cape Town%')
+    GROUP BY
+        skills_dim.skill_id
+),
+ average_salary AS (
+--Query 4 modified
+    SELECT 
+        skills_job_dim.skill_id,
+        --skills,
+        ROUND(AVG(salary_year_avg), 2) AS avg_salary
+    FROM 
+        job_postings_fact
+    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON skills_dim.skill_id = skills_job_dim.skill_id
+    WHERE
+        job_title_short = 'Data Analyst'
+        AND salary_year_avg IS NOT NULL
+    GROUP BY
+        skills_job_dim.skill_id
+)
+
+SELECT 
+   -- skills_demand.skill_id,
+    skills_demand.skills,
+    demand_count,
+    avg_salary
+FROM 
+    skills_demand
+INNER JOIN average_salary ON skills_demand.skill_id = average_salary.skill_id
+WHERE 
+    demand_count > 10
+ORDER BY
+    avg_salary DESC,
+    demand_count DESC
+LIMIT 25;
+
+
+-- Shorter more concise query
+SELECT
+    skills_dim.skill_id,
+    skills_dim.skills,
+    COUNT(skills_job_dim.job_id) AS demand_count,
+    ROUND(AVG(salary_year_avg), 2) AS avg_salary
+FROM 
+    job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_dim.skill_id = skills_job_dim.skill_id
+WHERE
+    job_title_short = 'Data Analyst'
+    AND salary_year_avg IS NOT NULL
+    AND (job_work_from_home = True OR job_location LIKE 'Cape Town%')
+GROUP BY
+    skills_dim.skill_id
+HAVING  
+    COUNT(skills_job_dim.job_id) > 10
+ORDER BY
+    avg_salary DESC,
+    demand_count DESC
+LIMIT 25;
